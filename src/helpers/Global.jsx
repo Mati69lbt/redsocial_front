90d27bf89eb55e4e74a11427a18ec569216396b4
Mati69lbt <mati69_lbt@hotmail.com>
2024-03-06 12:30:07 -0300

    deploy1

--- src/helpers/Global.jsx
+++ src/helpers/Global.jsx
@@ -1,3 +1,4 @@
 export const Global = {
-  url_backend: import.meta.env.VITE_URL_BACKEND,
+  // url_backend: import.meta.env.VITE_URL_BACKEND,
+  url_backend: import.meta.env.VITE_BACKEND_DEPLOY,
 };
